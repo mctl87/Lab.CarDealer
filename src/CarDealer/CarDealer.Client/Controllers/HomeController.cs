bd87296572731d58b560adada9e30f72900c305c
Michal Teler <Michal.Teler@equiniti.com>
2022-02-04 09:01:33 +0100

    initial commit

---
+++ src/CarDealer/CarDealer.Client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CarDealer.Client.Data;
+using CarDealer.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarDealer.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(CarContext.Cars);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
